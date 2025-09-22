aaf325637e435a8780fbdb8c26c083e1331be202
domsmutku <zientek.nataliaa@gmail.com>
2025-09-22 17:46:46 +0200

    Add API and integrate AI

--- src/javascript.js
+++ src/javascript.js
@@ -1,12 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "sun-drenched meadows whisper tales of slow, quiet days",
+    strings: response.data.answer,
     autoStart: true,
     delay: 80,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#input");
+  let apiKey = "83a222ot49ddfde0622ffadcc7bfbdb5";
+  let prompt = `User instructions: generate a poem in English about ${instructionsInput.value}`;
+  let context =
+    "You are a poem expert. You like to write nostalgic poems in the style of James Joyce. You should generate a 4 line poem and seperate each line with <br/>. For each poem create a matching title and include it at the beginning inside <strong>. Seperate the title from the poem with a <br/>. Make sure to follow the user instructions.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-form");
