730c5ff31295a29ca0c7396a9428e48882ca0941
domsmutku <zientek.nataliaa@gmail.com>
2025-09-22 18:15:47 +0200

    Improve user experience

--- src/javascript.js
+++ src/javascript.js
@@ -17,6 +17,9 @@ function generatePoem(event) {
     "You are a poem expert. You like to write nostalgic poems in the style of James Joyce. You should generate a 4 line poem and seperate each line with <br/>. For each poem create a matching title and include it at the beginning inside <strong>. Seperate the title from the poem with a <br/>. Make sure to follow the user instructions.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.innerHTML = "Give me but a moment whilst I weave thy verses!";
+
   axios.get(apiUrl).then(displayPoem);
 }
 
